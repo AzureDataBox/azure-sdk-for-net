17160fb0551207f5c4d1e1eb6a17773d4ca040a9
t-vajana <t-vajana@microsoft.com>
2020-06-01 10:20:10 +0530

    Create new DataBox SDK project

---
+++ sdk/databox/Azure.Management.DataBox/src/autorest.md
@@ -0,0 +1,11 @@
+# Generated code configuration
+
+Run `dotnet msbuild /t:GenerateCode` to generate code.
+
+``` yaml
+
+azure-arm: true
+require: https://github.com/t-vajana/azure-rest-api-specs/blob/dcb88c29a56bb1d878cccb5a07338e3338496d9d/specification/databox/resource-manager/readme.md
+modelerfour:
+  lenient-model-deduplication: true
+```
---
+++ sdk/databox/Azure.Management.DataBox/tests/Microsft.DataBoxManagementTestEnvironment.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core.TestFramework;
+
+namespace Azure.Management.DataBox.Tests
+{
+    public class Microsft.DataBoxManagementTestEnvironment : TestEnvironment
+    {
+        public Microsft.DataBoxManagementTestEnvironment() : base("microsft.databoxmgmt")
+        {
+        }
+    }
+}
